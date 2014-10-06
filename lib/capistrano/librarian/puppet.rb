696640fa652faff7ade1843d0eab9a16cf80d977
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-10-07 03:53:22 +0900

    Initial commit

---
+++ lib/capistrano/librarian/puppet.rb
@@ -0,0 +1,9 @@
+require "capistrano/librarian/puppet/version"
+
+module Capistrano
+  module Librarian
+    module Puppet
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/librarian/puppet/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Librarian
+    module Puppet
+      VERSION = "0.0.1"
+    end
+  end
+end
